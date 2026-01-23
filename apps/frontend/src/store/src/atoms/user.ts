1a2f5e04cfd23f4e184b66840b05fca637280438
Lakshya Nebwani <lakshya.nebhwani@gmail.com>
2026-01-23 19:16:15 +0530

    update BACKEND_URL to use environment variable for configuration

--- apps/frontend/src/store/src/atoms/user.ts
+++ apps/frontend/src/store/src/atoms/user.ts
@@ -1,6 +1,6 @@
 import {atom, selector} from "recoil";
 
-export const BACKEND_URL = "http://localhost:3000";
+export const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
 
 export interface User{
     token: string;

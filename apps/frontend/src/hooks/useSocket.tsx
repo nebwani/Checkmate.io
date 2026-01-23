9623e490c18ffd46e7aa2712f628171c17775740
Lakshya Nebwani <lakshya.nebhwani@gmail.com>
2026-01-23 17:36:59 +0530

    update useSocket and Login components to use environment variables for WebSocket and backend URLs

--- apps/frontend/src/hooks/useSocket.tsx
+++ apps/frontend/src/hooks/useSocket.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { useUser } from "../store/src/hooks/useUser";
 
-const WS_URL = "ws://localhost:8080"
+const WS_URL = import.meta.env.VITE_WS_URL;
 
 export const useSocket = () => {
     const [socket, setSocket] = useState<WebSocket | null>(null);
